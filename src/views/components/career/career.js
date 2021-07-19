24a9c06f96955f07d0ffc9438c58c2bae68ef8bb
vwvwMM <vincentwu0628@gmail.com>
2021-07-20 00:19:48 +0800

    add main career page

---
+++ src/views/components/career/career.js
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './career.css'
+import Recruitment_image from './Recruitment.png'
+import Recommendation_image from './Recommendation.png'
+
+const Career = () => {
+  return (
+    <div className="align-items-center d-flex career_link_imgs">
+      <div className="row container mx-auto ">
+        <div className="col d-flex justify-content-center">
+          <Link to="">
+            <img className="career_img img-fluid" src={Recruitment_image} alt="Recruitment" />
+          </Link>
+        </div>
+        <div className="col d-flex justify-content-center">
+          <Link to="">
+            <img className="career_img img-fluid" src={Recommendation_image} alt="Recommendation" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Career
