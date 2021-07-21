9ff00753750ec6c898f66ecd6b4b817acb3a9cc8
vwvwMM <vincentwu0628@gmail.com>
2021-07-21 18:40:01 +0800

    finish recruitment

--- src/views/components/career/recruitment/Post.js
+++ src/views/components/career/recruitment/Post.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import { CWidgetBrand, CRow, CCol } from '@coreui/react'
 import eesa from './eesa-icon.png'
 import './Recruitment.css'
+import Masonry from 'react-masonry-css'
 
 const Post = ({ data }) => {
   const spec = (li) => {
@@ -15,15 +16,13 @@ const Post = ({ data }) => {
   }
   const posts = data.map((post) => {
     return (
-      <CCol sm="12" lg="6" key={post.id} className="RecruBlock col">
-        <CRow className="container">
-          <CWidgetBrand
-            className="mb-4 widgetbrand"
-            headerChildren={<img className="eesa" src={eesa} alt="eesa" />}
-            values={[[post.company_name]]}
-          />
-        </CRow>
-        <CRow className="container">
+      <div className="RecruBlock" key={post.id}>
+        <CWidgetBrand
+          className="mb-4 widgetbrand"
+          headerChildren={<img className="eesa" src={eesa} alt="eesa" />}
+          values={[[post.company_name]]}
+        />
+        <div className="recrucontent">
           <h3 style={{ 'font-weight': '600' }}>{post.title.title}</h3>
           <h2 style={{ margin: '1rem 0rem', fontWeight: '600', color: 'red' }}>
             {post.info.salary}
@@ -35,11 +34,31 @@ const Post = ({ data }) => {
           <h4>{post.spec.requirement.map((req) => spec(req))}</h4>
           <h3 style={{ 'font-weight': '600', margin: '1rem 0 0.1rem' }}>說明：</h3>
           <h4>{post.spec.description.map((des) => spec(des))}</h4>
-        </CRow>
-      </CCol>
+        </div>
+      </div>
     )
   })
-  return <CRow>{posts}</CRow>
+  const breakpointColumnsObj = {
+    default: 2,
+    1100: 2,
+    700: 1,
+    500: 1,
+  }
+  return (
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+      columnAttrs={{
+        className: 'should be overridden',
+        'data-test': '',
+        style: { '--test': 'test' },
+      }}
+      style={{ display: 'flex' }}
+    >
+      {posts}
+    </Masonry>
+  )
 }
 Post.propTypes = {
   data: PropTypes.array,
