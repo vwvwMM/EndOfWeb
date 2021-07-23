d75181a5c497657a84c477757ed21afbd3fb5172
vwvwMM <vincentwu0628@gmail.com>
2021-07-23 18:36:04 +0800

    rebase profile

--- src/_navIn.js
+++ src/_navIn.js
@@ -52,6 +52,17 @@ const _navIn = [
     },
   },
   {
+    _component: 'CNavItem',
+    as: NavLink,
+    anchor: 'About',
+    to: '/abo',
+    icon: <CIcon name="cil-notes" customClassName="nav-icon" />,
+    badge: {
+      color: 'info',
+      text: 'NEW',
+    },
+  },
+  {
     _component: 'CNavGroup',
     anchor: 'Pages',
     icon: <CIcon name="cil-star" customClassName="nav-icon" />,
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Column = React.lazy(() => import('./views/components/column/Column'))
+const About = React.lazy(() => import('./views/components/about/About'))
 const ShowColumns = React.lazy(() => import('./views/components/columnSummary/showColumns'))
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const Career = React.lazy(() => import('./views/components/career/career'))
@@ -13,6 +14,7 @@ const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/column', name: 'Column', component: Column },
+  { path: '/about', name: 'About', component: About },
   { path: '/columnSummary', name: 'ColumnSummary', component: ShowColumns },
   { path: '/career', name: 'Career', component: Career },
   { path: '/recommendation', name: 'Recommendation', component: Recommendation },
