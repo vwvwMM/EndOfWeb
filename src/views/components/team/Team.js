ab9dd4ca29185209c57d336127e9cf58fd2b46a7
vwvwMM <vincentwu0628@gmail.com>
2021-07-23 12:24:10 +0800

    create team db

---
+++ src/views/components/team/Team.js
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+import React, { useState, useEffect } from 'react'
+import TeamBlocks from './TeamBlocks'
+const Team = () => {
+  const [data, setData] = useState([])
+  const getData = () => {
+    fetch('historyData.json', {
+      headers: {
+        ContentType: 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+  return (
+    <></>
+    // <div style={{ width: '100%' }}>{data.history && data.member && <Timeline data={data} />}</div>
+  )
+}
+
+export default Team
