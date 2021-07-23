c90d4218a6e4c6e7d07bac3668c6784c5cb7a47c
vwvwMM <vincentwu0628@gmail.com>
2021-07-23 15:32:15 +0800

    correct photos

--- src/views/components/team/TeamBlocks.js
+++ src/views/components/team/TeamBlocks.js
@@ -8,7 +8,9 @@ const TeamBlocks = ({ data }) => {
     return (
       <CCol xs={2} key={person.name} align="center" className="justify-content-center">
         <CImage src={person.img} height="200rem" className="img-fluid" />
-        <h3>{person.name}</h3>
+        <h3 className="mb-4">
+          {person.name} {person.part}
+        </h3>
       </CCol>
     )
   })
@@ -16,7 +18,7 @@ const TeamBlocks = ({ data }) => {
     return (
       <CCol xs={2} key={person.name} align="center" className="justify-content-between">
         <CImage src={person.img} height="200rem" className="img-fluid" />
-        <h3>{person.name}</h3>
+        <h3 className="mb-4">{person.name}</h3>
       </CCol>
     )
   })
@@ -24,7 +26,7 @@ const TeamBlocks = ({ data }) => {
     return (
       <CCol xs={2} key={person.name} align="center" className="justify-content-center">
         <CImage src={person.img} height="200rem" className="img-fluid" />
-        <h3>{person.name}</h3>
+        <h3 className="mb-4">{person.name}</h3>
       </CCol>
     )
   })
@@ -32,27 +34,27 @@ const TeamBlocks = ({ data }) => {
     return (
       <CCol xs={2} key={person.name} align="center" className="justify-content-center">
         <CImage src={person.img} height="200rem" className="img-fluid" />
-        <h3>{person.name}</h3>
+        <h3 className="mb-4">{person.name}</h3>
       </CCol>
     )
   })
   return (
     <>
       <CCol>
-        <CRow className="justify-content-around mt-4" style={{ marginBottom: '20rem' }} xs={5}>
-          <h2 className="mb-4">負責人：</h2>
+        <CRow className="justify-content-around mt-4 mb-4" style={{ marginBottom: '20rem' }} xs={5}>
+          <h3 className="mb-4">負責人：</h3>
           {minister}
         </CRow>
-        <CRow className="justify-content-around mt-4 " xs={5}>
-          <h2 className="mb-4">網頁前端團隊：</h2>
+        <CRow className="justify-content-around mt-4 mb-4 " xs={5}>
+          <h3 className="mb-4">網頁前端團隊：</h3>
           {front}
         </CRow>
-        <CRow className="justify-content-around mt-4 " xs={5}>
-          <h2 className="mb-4">網頁後端團隊：</h2>
+        <CRow className="justify-content-around mt-4 mb-4 " xs={5}>
+          <h3 className="mb-4">網頁後端團隊：</h3>
           {back}
         </CRow>
-        <CRow className="justify-content-around mt-4 " xs={5}>
-          <h2 className="mb-4">留學資料蒐集團隊：</h2>
+        <CRow className="justify-content-around mt-4 mb-4 " xs={5}>
+          <h3 className="mb-4">留學資料蒐集團隊：</h3>
           {abroad}
         </CRow>
       </CCol>
