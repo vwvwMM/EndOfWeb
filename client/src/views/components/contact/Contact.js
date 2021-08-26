cee2a06d6a978d7e7920c7e7f66e799f6cfde5d7
vwvwMM <vincentwu0628@gmail.com>
2021-08-26 11:32:33 +0800

    contact us

--- client/src/views/components/contact/Contact.js
+++ client/src/views/components/contact/Contact.js
@@ -1,16 +1,14 @@
 /* eslint-disable prettier/prettier */
 import { CCol, CContainer, CRow, CButton } from '@coreui/react'
 import React from 'react'
-import { Link } from 'react-router-dom'
-import left_img from '../../../assets/images/left_minister.png'
-import right_img from '../../../assets/images/right_minister.png'
 
 const Contact = () => {
   return (
     <CContainer className="text-center">
+      <h1>Contact Us</h1>
       <CRow className="justify-content-evenly">
         <CCol xs={3}>
-          <i class="bi bi-instagram"></i>&emsp;<a href="https://www.instagram.com/ntueeplus/">IG</a>
+          <i className="bi bi-instagram"></i>&emsp;<a href="https://www.instagram.com/ntueeplus/">IG</a>
         </CCol>
         <CCol xs={3}>
           <h5>QRCode</h5>
@@ -18,7 +16,7 @@ const Contact = () => {
       </CRow>
       <CRow className="justify-content-evenly">
         <CCol xs={3}>
-          <i class="bi bi-facebook"></i>&emsp;<a href="https://www.facebook.com/groups/ntueeplus">FB</a>
+          <i className="bi bi-facebook"></i>&emsp;<a href="https://www.facebook.com/groups/ntueeplus">FB</a>
         </CCol>
         <CCol xs={3}>
           <h5>QRCode</h5>
@@ -26,7 +24,7 @@ const Contact = () => {
       </CRow>
       <CRow className="justify-content-evenly">
         <CCol xs={3}>
-          <i class="bi bi-github"></i>&emsp;<a href="https://github.com/NTUEE-PLUS/EndOfWeb">Github</a>
+          <i className="bi bi-github"></i>&emsp;<a href="https://github.com/NTUEE-PLUS/EndOfWeb">Github</a>
         </CCol>
         <CCol xs={3}>
           <h5>QRCode</h5>
