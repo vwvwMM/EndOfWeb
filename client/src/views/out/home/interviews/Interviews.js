60126e6ac855e23bd6ba8580a5924dba454bcbe9
vwvwMM <vincentwu0628@gmail.com>
2021-09-15 01:24:04 +0800

    finish outline words

--- client/src/views/out/home/interviews/Interviews.js
+++ client/src/views/out/home/interviews/Interviews.js
@@ -1,6 +1,9 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
 import { CContainer } from '@coreui/react'
+import columnOutline1999 from '../../../../assets/images/1999_column_outline.jpg'
+import columnOutline2008 from '../../../../assets/images/2008_column_outline.jpg'
+import columnOutline2012 from '../../../../assets/images/2012_column_outline.jpg'
 
 const Interviews = () => {
   return (
@@ -24,14 +27,12 @@ const Interviews = () => {
                     <h3>2012級 李昀樵 </h3>
                     <h4>17直播 技術副總</h4>
                     <p className="mx-3">
-                      李昀樵曾是李琳山教授語音實驗室的研究生，研究所時期除了研究外，更有進行小型創業，開發出新聞摘要軟件及搜尋公共腳踏車的APP，前者以兩萬元售出，後者成為該領域當時市占率最高的APP。之後，李昀樵加入17直播，監管每年五百萬的IT預算，帶領團隊從5人成長到107人，更將17
-                      Media的首屏載入速度從4秒縮減到0.3秒，成為同業中最快速的。李昀樵在新創領域有豐富經驗，現更到Harvard
-                      MBA進修。
+                      李昀樵曾是李琳山教授語音實驗室的研究生，研究所時期除了研究外，更有進行小型創業，開發出新聞摘要軟件及搜尋公共腳踏車的APP
                     </p>
                   </div>
                   <img
-                    src="https://picsum.photos/350/500"
-                    className="img-responsive"
+                    src={columnOutline2012}
+                    className="img-fluid p-2 rounded"
                     alt="Project Title"
                   />{' '}
                 </a>{' '}
@@ -52,17 +53,12 @@ const Interviews = () => {
                     <h4> (CEO/ Founder @ Ganzin Technology, Prof. @ NTUEE)</h4>
                     <p className="mx-3">
                       簡韶逸教授任職於台大電子所長達16年，致力於多媒體訊號處理系統、多媒體積體電路設計、晶片系統設計方法的研究。
-                      多年來，「媒體晶片系統實驗室」不斷研發出優異的技術。2016
-                      年簡韶逸教授帶學生做出眼球追蹤技術的雛形時，他看好一定能應用在 AR/VR
-                      上，於是決心創業。2018年1月時見臻科技(Ganzin Technology) 從台灣大學spin
-                      off。見臻科技提供整合性視線追蹤解決方案 (eye tracking
-                      solution)，包括視線追蹤模組 (eye tracking module) 及對應之軟體開發平台
-                      (SDK)，解決目前市面上眼動儀成本高、功耗高、體積大、機構複雜的問題。
+                      多年來，「媒體晶片系統實驗室」不斷研發出優異的技術。
                     </p>
                   </div>
                   <img
-                    src="https://picsum.photos/350/500"
-                    className="img-responsive"
+                    src={columnOutline1999}
+                    className="img-fluid rounded"
                     alt="Project Title"
                   />{' '}
                 </a>{' '}
@@ -83,16 +79,12 @@ const Interviews = () => {
                     <h4> (Technical Lead Manager @ Google Brain)</h4>
                     <p className="mx-3">
                       目前任職於 Google Brain
-                      的團隊技術領導者和軟體主管工程師的鄭恆之，從事大規模機器學習的研究與軟體開發。在2013年加入
-                      Googe
-                      的實習生行列進行廣告排行的研究，僅僅花費不到三年的時間，就從實習生轉正職於
-                      Google Research 的軟體工程師並且晉升 Google Brain
-                      的技術領導與主管工程師，亮麗的職涯經歷背後是堅實的學術基礎與多篇國際期刊論文的支撐。
+                      的團隊技術領導者和軟體主管工程師的鄭恆之，從事大規模機器學習的研究與軟體開發。
                     </p>
                   </div>
                   <img
-                    src="https://picsum.photos/350/500"
-                    className="img-responsive"
+                    src={columnOutline2008}
+                    className="img-fluid p-2 rounded"
                     alt="Project Title"
                   />{' '}
                 </a>{' '}
