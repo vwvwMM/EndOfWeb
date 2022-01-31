1cc8730c20f552658ee91483001a57b911f1ee9b
vwvwMM <vincentwu0628@gmail.com>
2022-01-31 17:37:36 +0800

    finish route

---
+++ client/src/views/auth/register/Register.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+
+const Register = () => {
+  return <div>Register</div>;
+};
+
+export default Register;
---
+++ client/src/views/auth/register/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable prettier/prettier */
+import Register from "./Register";
+export default Register
