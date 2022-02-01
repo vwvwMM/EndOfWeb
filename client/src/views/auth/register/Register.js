3b0701f317bb203b5e263ed1156fa075f559831f
vwvwMM <vincentwu0628@gmail.com>
2022-02-02 00:19:56 +0800

    connect to api

--- client/src/views/auth/register/Register.js
+++ client/src/views/auth/register/Register.js
@@ -1,56 +1,55 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Applicant from './Applicant'
-import {
-  CModal,
-  CModalBody,
-  CModalContent,
-  CModalHeader,
-  CModalFooter,
-  CModalTitle,
-  CButton,
-} from '@coreui/react'
+import axios from 'axios'
+import { CModal, CModalBody, CModalHeader, CModalFooter, CModalTitle, CButton } from '@coreui/react'
+import { Spinner } from './index'
 const Register = () => {
-  const [isModal, setIsModal] = useState(false)
+  const [isModal, setIsModal] = useState(true)
   const [modalPerson, setModalPerson] = useState({
     username: '',
     account: '',
     email: '',
     imgSrc: '',
   })
-  const [applicants, setApplicants] = useState([
-    {
-      username: '123',
-      account: 'b09901022',
-      email: 'vincentwu0628@gmail.com',
-      imgSrc:
-        'https://lh3.googleusercontent.com/WMiB19W30Hv7ZncOPnbe-5m1QrsytApHzgi_69jjz3OD_0wuKoIav7-12JY3uwa0EshTy2N06VCwUdTiD1HUkB2nEFpnZQpeqy_H09E=s130',
-    },
-    {
-      username: '456',
-      account: 'b09901122',
-      email: 'vincentwu0028@gmail.com',
-      imgSrc:
-        'https://lh3.googleusercontent.com/WMiB19W30Hv7ZncOPnbe-5m1QrsytApHzgi_69jjz3OD_0wuKoIav7-12JY3uwa0EshTy2N06VCwUdTiD1HUkB2nEFpnZQpeqy_H09E=s130',
-    },
-    {
-      username: '13423535345',
-      account: 'b09901029',
-      email: 'vincentwu064323535523428@gmail.com',
-      imgSrc:
-        'https://lh3.googleusercontent.com/jSniiLwosYg4YNa5ZJg4S115fP-vZ570m5Mrup117XaGUKiN2VW_vK4tYPXmaYJ1ku3pQqeMNdZWdXAvCXpH6QAckALelFdTGtgcDbY=s550',
-    },
-  ])
+  const [isPending, setIsPending] = useState(false)
+  const [applicants, setApplicants] = useState([])
+  const getPendings = () => {
+    setIsPending(true)
+    axios
+      .post('/api/showPending')
+      .then((res) => {
+        console.log('pendings:', res.data.pendings.length)
+        setApplicants(res.data.pendings)
+        setIsPending(false)
+      })
+      .catch((err) => console.log(err))
+  }
   const verify = () => {
-    alert('已發送驗證信至對方信箱！')
-    setIsModal(false)
+    axios
+      .post('/api/handlePending', { account: modalPerson.account, acceptUser: true })
+      .then((res) => {
+        alert('已發送驗證信至對方信箱！')
+        setIsModal(false)
+      }).catch(err=>console.log(err))
+    }
+    const reject = () => {
+    axios
+      .post('/api/handlePending', { account: modalPerson.account, acceptUser: false })
+      .then((res) => {
+        alert('已發送驗證信至對方信箱！')
+        setIsModal(false)
+        // alert('已發送拒絕申請信至對方信箱！')
+      }).catch(err=>console.log(err))
   }
-  const reject = () => {
-    alert('已發送拒絕申請信至對方信箱！')
-    setIsModal(false)
-  }
-  return (
+  useEffect(() => {
+    getPendings()
+  }, [])
+  return isPending ? (
+    <Spinner />
+  ) : (
     <>
+      <CModal></CModal>
       <CModal size="lg" visible={isModal} onDismiss={() => setIsModal(false)} alignment="center">
         <CModalHeader onDismiss={() => setIsModal(false)}>
           <CModalTitle>驗證身分</CModalTitle>
@@ -62,7 +61,7 @@ const Register = () => {
           <br />
           信箱：{modalPerson.email}
           <br />
-          <img src={modalPerson.imgSrc} alt="" style={{ width: '48rem', margin:'1rem' }} />
+          <img src={modalPerson.imgSrc} alt="" style={{ width: '48rem', margin: '1rem' }} />
         </CModalBody>
         <CModalFooter>
           <CButton color="warning" onClick={reject}>
@@ -74,10 +73,21 @@ const Register = () => {
         </CModalFooter>
       </CModal>
       <div className="auth-register mx-auto w-75 py-3">
-        <h1 className="mt-3">底下為待您確認的註冊申請：</h1>
-        {applicants.map((a, i) => (
-          <Applicant person={a} key={i} setIsModal={setIsModal} setModalPerson={setModalPerson} />
-        ))}
+        {applicants.length ? (
+          <>
+            <h1 className="mt-3">底下為待您確認的註冊申請：</h1>
+            {applicants.map((a, i) => (
+              <Applicant
+                person={a}
+                key={i}
+                setIsModal={setIsModal}
+                setModalPerson={setModalPerson}
+              />
+            ))}
+          </>
+        ) : (
+          <h1>目前無新的確認申請！</h1>
+        )}
       </div>
     </>
   )
