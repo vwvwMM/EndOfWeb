f6469cfde48cc00f6cea9e4e145bc497d4557533
VincentWu <vincentwu0628@gmail.com>
2023-08-25 20:45:22 +0800

    NTUEEPLUS-293. Enhance Secuity (#251)
    * only allow cloudflare ip
    
    * use env for session key and smtp password
    
    * add session key and smtp password into env

--- backend/routes/middleware/mail/credential.js
+++ backend/routes/middleware/mail/credential.js
@@ -3,6 +3,6 @@ module.exports = {
   // cookieSecret: 'your cookie secret goes here',
   gmail: {
     user: 'ntueeplus2020@gmail.com',
-    password: 'upvnqetmnibmnaac', //這不是真正的密碼，不用試了:)
+    password: process.env.GMAIL_PASSWORD, //這不是真正的密碼，不用試了:)
   },
 }
